e9732b268e9edc7ffe1fb6393cdaa0b0037bb235
adrianpacholec <adrianpacholec@gmail.com>
2017-12-22 16:28:46 +0100

    wczytywanie translacji z pliku

--- LSR/LabelSwtichingRouter/LabelSwtichingRouter/ConnectionController.cs
+++ LSR/LabelSwtichingRouter/LabelSwtichingRouter/ConnectionController.cs
@@ -10,38 +10,49 @@ namespace LabelSwitchingRouter
     class ConnectionController
     {
         private FIB fib;
-        private List<(int port, string address)> addressTranslation;
+        private List<string[]> addressTranslation; //IPaddress - localPort
         public ConnectionController(FIB fib)
         {
             this.fib = fib;
-            addressTranslation.Add((1, "192.0.3.2"));
-            addressTranslation.Add((2, "192.0.3.8"));
-            addressTranslation.Add((3, "192.0.3.7"));
-            addressTranslation.Add((4, "192.0.3.5"));
-            addressTranslation.Add((5, "192.0.3.1"));
-            addressTranslation.Add((6, "192.0.3.5"));
-            addressTranslation.Add((7, "192.0.3.4"));
+            LoadTransationTableFromFile();
+        }
+
+        private void LoadTransationTableFromFile()
+        {
+            string fileName = Config.getProperty("AddressTranslationTableFileName");
+            string[] loadedFile = LoadFile(fileName);
+            string[] snppParams = null;
+            foreach (string str in loadedFile)
+            {
+                snppParams = str.Split(' ');
+                addressTranslation.Add(snppParams);
+            }
+        }
+
+        private string[] LoadFile(String fileName)
+        {
+            string[] fileLines = System.IO.File.ReadAllLines(fileName);
+            return fileLines;
         }
 
         private bool ConnectionRequestIn(SNP pathBegin, SNP pathEnd)
         {
             int inputPort=0, outputPort=0;
-            foreach ((int port, string address) translation in addressTranslation)
+            foreach (string[] translation in addressTranslation)
             {
-                if (translation.address == pathBegin.Address)
+                if (translation[0] == pathBegin.Address)
                 {
-                    inputPort = translation.port;
+                    inputPort = Int32.Parse(translation[1]);
                 }
-                else if (translation.address == pathEnd.Address)
+                else if (translation[0] == pathEnd.Address)
                 {
-                    outputPort = translation.port;
+                    outputPort = Int32.Parse(translation[1]);
                 }
             }
-
             fib.AddEntry(inputPort, pathBegin.Label, outputPort, pathEnd.Label, 0, 0, "0");
-           
             return true;
-
         }
     }
 }
+
+           
